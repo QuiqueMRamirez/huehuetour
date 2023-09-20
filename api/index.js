8aedf71fb39264aab5aaf3623da0a8299bb50c71
cmazariegosramirez <cmazariegosramirez@gmail.com>
2023-09-19 18:07:08 -0600

    s3 added to index, api url renamed

--- api/index.js
+++ api/index.js
@@ -12,6 +12,7 @@ const multer = require('multer');
 const fs = require('fs');
 const Booking = require("./models/Booking");
 const Review = require("./models/Review");
+const {S3Client, PutObjectCommand} = require('@aws-sdk/client-s3');
 
 require("dotenv").config();
 const bcryptSalt = bcrypt.genSaltSync(10);
@@ -27,10 +28,28 @@ app.use(
 );
 
 mongoose.connect(process.env.MONGO_URL);
+const bucketName = "touringimages"
 
-app.get("/test", (req, res) => {
-  res.json("ok");
-});
+async function uploadToS3(path, originalFilename, mimetype) {
+  const client = new S3Client({
+    region: 'us-east-1',
+    credentials: {
+      accessKeyId: process.env.S3_ACCESS_KEY,
+      secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+    },
+  });
+  const parts = originalFilename.split('.');
+  const ext = parts[parts.length - 1];
+  const newFilename = Date.now() + '.' + ext;
+  await client.send(new PutObjectCommand({
+    Bucket: bucketName,
+    Body: fs.readFileSync(path),
+    Key: newFilename,
+    ContentType: mimetype,
+    ACL: 'public-read',
+  }));
+  return `https://${bucket}.s3.amazonaws.com/${newFilename}`;
+}
 
 function getUserDataFromReq(req) {
   return new Promise((resolve, reject) =>{
@@ -42,7 +61,7 @@ function getUserDataFromReq(req) {
   
 }
 
-app.post("/register", async (req, res) => {
+app.post("/api/register", async (req, res) => {
   const { name, email, photo, password } = req.body;
   try {
     const userDoc = await User.create({
@@ -57,7 +76,7 @@ app.post("/register", async (req, res) => {
   }
 });
 
-app.post('/login', async (req, res) => {
+app.post('/api/login', async (req, res) => {
   const {email, password} = req.body;
   try{
     const userDoc = await User.findOne({email})
@@ -80,7 +99,7 @@ app.post('/login', async (req, res) => {
   }
 })
 
-app.get('/profile', (req, res) => {
+app.get('/api/profile', (req, res) => {
   const {token} = req.cookies;
   if(token){
     jwt.verify(token, jwtSecret, {}, (err, user) => {
@@ -92,35 +111,33 @@ app.get('/profile', (req, res) => {
   }
 })
 
-app.post('/logout', (req, res) => {
+app.post('/api/logout', (req, res) => {
   res.cookie('token','').json(true)
 })
 
-app.post('/upload-by-link', async(req, res) => {
-  const {link} = req.body
-  const newName = 'photo' + Date.now() + '.jpg'
+app.post('/api/upload-by-link', async(req, res) => {
+  const {link} = req.body;
+  const newName = 'photo' + Date.now() + '.jpg';
   await imageDownloader.image({
     url: link,
-    dest: __dirname + '/uploads/' + newName
-  })
-  res.json(newName)
+    dest: '/tmp/' +newName,
+  });
+  const url = await uploadToS3('/tmp/' +newName, newName, mime.lookup('/tmp/' +newName));
+  res.json(url);
 })
 
-const photosMiddleware = multer({dest:'uploads'})
-app.post('/upload', photosMiddleware.array('photos',100),(req, res) => {
-  const uploadedFiles = []
+const photosMiddleware = multer({dest:'/tmp'});
+app.post('/api/upload', photosMiddleware.array('photos', 100), async (req,res) => {
+  const uploadedFiles = [];
   for (let i = 0; i < req.files.length; i++) {
-    const {path, originalname} = req.files[i]
-    const parts = originalname.split('.')
-    const ext = parts[parts.length - 1]
-    const newPath = path + '.' + ext
-    fs.renameSync(path, newPath)
-    uploadedFiles.push(newPath.replace('uploads/',''))
+    const {path,originalname,mimetype} = req.files[i];
+    const url = await uploadToS3(path, originalname, mimetype);
+    uploadedFiles.push(url);
   }
-  res.json(uploadedFiles)
-})
+  res.json(uploadedFiles);
+});
 
-app.post('/places', (req, res) => {
+app.post('/api/places', (req, res) => {
   const {token} = req.cookies;
   const {title, address, addedPhotos, description, placeType, perks, checkIn, checkOut, maxGuests, extraInfo, price} = req.body;
   jwt.verify(token, jwtSecret, {}, async (err, userData) => {
@@ -136,7 +153,7 @@ app.post('/places', (req, res) => {
   
 })
 
-app.get('/user-places', (req, res) => {
+app.get('/api/user-places', (req, res) => {
   const {token} = req.cookies;
   jwt.verify(token, jwtSecret, {}, async (err, userData) => {
     if(err) console.log(err)
@@ -145,7 +162,7 @@ app.get('/user-places', (req, res) => {
   })
 })
 
-app.get('/places/:id', async(req, res) => {
+app.get('/api/places/:id', async(req, res) => {
   const {id} = req.params
   const placeById = await Place.findById(id)
   if(placeById){
@@ -166,7 +183,7 @@ app.get('/places/:id', async(req, res) => {
   
 })
 
-app.put('/places', async(req, res) => {
+app.put('/api/places', async(req, res) => {
   const {token} = req.cookies;
   const {id, title, address, addedPhotos, description, perks, checkIn, checkOut, maxGuests, extraInfo, price} = req.body;
   jwt.verify(token, jwtSecret, {}, async (err, userData) => {
@@ -182,16 +199,16 @@ app.put('/places', async(req, res) => {
   })
 })
 
-app.get('/places', async(req, res) => {
+app.get('/api/places', async(req, res) => {
   res.json(await Place.find())
 })
 
-app.get('/placesByFilter/:id', async(req, res) => {
+app.get('/api/placesByFilter/:id', async(req, res) => {
   const {id} = req.params
   res.json(await Place.find({placeType: id === 'Hospedajes' ? 'H' : 'A'}))
 })
 
-app.post('/bookings', async(req, res) => {
+app.post('/api/bookings', async(req, res) => {
   const userData = await getUserDataFromReq(req)
   let {checkIn, checkOut, place, numberOfGuests, name, phone, price, owner} = req.body
   try{
@@ -206,18 +223,18 @@ app.post('/bookings', async(req, res) => {
 
 
 
-app.get('/bookings', async(req, res) => {
+app.get('/api/bookings', async(req, res) => {
   const userData = await getUserDataFromReq(req)
   res.json(await Booking.find({user:userData.id}).populate('place'))
 })
 
-app.get('/bookingsByOwnUser', async(req, res) => {
+app.get('/api/bookingsByOwnUser', async(req, res) => {
   const userData = await getUserDataFromReq(req)
   const bookingData = await Booking.find({ownerUser:userData.id}).populate('place')
   res.status(200).json(bookingData)
 })
 
-app.post('/review/:id', async(req, res) => {
+app.post('/api/review/:id', async(req, res) => {
   const userData = await getUserDataFromReq(req)
   const {id} = req.params
   const {title, content} = req.body
@@ -233,7 +250,7 @@ app.post('/review/:id', async(req, res) => {
   }
 })
 
-app.delete('/bookings/:id', async(req, res) => {
+app.delete('/api/bookings/:id', async(req, res) => {
   const userData = await getUserDataFromReq(req)
   const {id} = req.params
   try{
